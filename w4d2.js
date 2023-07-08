99e3fd238d85c52eac01dbe97ba92fffc75df75a
Romy <romyjeanpierre@yahoo.com>
2023-07-08 17:49:57 -0400

    total recall part III completed

--- w4d2.js
+++ w4d2.js
@@ -245,24 +245,102 @@ myArray.shift();
 console.log(myArray);
 
 //Add the string "Bob Marley"to the beginning of the array.
-//Remove the string of your choice from the end of the array.
-//Reverse this array using Array.prototype.reverse(). Did you mutate the array? What does mutate mean? Did the .reverse()method return anything?
+myArray.unshift("Bob Marley");
+console.log(myArray);
 
+//Remove the string of your choice from the end of the array.
+myArray.splice(4, 1);
+console.log(myArray);
+
+//Reverse this array using Array.prototype.reverse(). Did you mutate the array? What does mutate mean? Did the .reverse()method return anything? the reverse method reverses the order of the array leaving the first element of the array last and the last becomes the first. The original array is now mutated. Mutate an array means the changing of the original array
+myArray.reverse();
+console.log(myArray);
 
 
 //* F. Biggie Smalls
-//Create a variable that contains an integer.
-
-//Write an if ... elsestatement that:
-
-//console.log()s "little number" if the number is entered is less than 100
-//console.log()s big numberif the number is greater than or equal to 100.
-
+/*Create a variable that contains an integer.
+Write an if ... else statement that:
+console.log()s "little number" if the number is entered is less than 100
+console.log()s big number if the number is greater than or equal to 100.*/
+let num2 = 444 
+if (num2 < 100) {
+    console.log("little number");
+} else  {
+    console.log("big number");
+}
 
 
 //* G. Monkey in the Middle
-//Write an if ... else if ... elsestatement:
+/*Write an if ... else if ... else statement:
+console.log()little number if the number entered is less than 5.
+If the number entered is more than 10, log big number.
+Otherwise, log "monkey".*/
+let childAge = 7
+if (childAge < 5) {
+    console.log('little number');
+} else if (childAge > 10) {
+    console.log('big number');
+}
+else {
+    console.log('monkey')
+}
 
-//console.log()little number if the number entered is less than 5.
-//If the number entered is more than 10, log big number.
-//Otherwise, log "monkey".
+//*H. What's in Your Closet?
+/*Below, we've given you examples of Kristyn and Thom's closets modeled as data in JavaScript.*/
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "Per Scholas hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+  
+  // Thom's closet is more complicated. Check out this nested data structure!!
+  const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
+ // What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+
+ console.log(`Kristyn is rocking that ${kristynsCloset[2]} today!`);
+
+//Kristyn just bought some sweet shades! Add "raybans"to her closet after "yellow knit hat".
+kristynsCloset[5] = ("raybans");
+console.log(kristynsCloset);
+
+//Kristyn spilled coffee on her hat... modify this item to read "stained knit hat"instead of yellow.
+kristynsCloset.splice(5, 1, "stained knit hat");
+console.log(kristynsCloset);
+
+//Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirtsarray.
+console.log (`${thomsCloset[0][0]}`);
+
+//In the same way, access one item from Thom's pants array.
+console.log (`${thomsCloset[1][1]}`);
+
+//Access one item from Thom's accessories array.
+console.log (`${thomsCloset[2][2]}`);
+
+//Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+console.log(`Thom is rocking a ${thomsCloset[0][0]}, ${thomsCloset[1][1]} and ${thomsCloset[2][2]}.`)
+
+//Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+thomsCloset[1][2] = 'Footie Pajamas';
+console.log(thomsCloset);
